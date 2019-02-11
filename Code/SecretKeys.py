2ef8ed656f1f41aac548bd03095ad17732b2d384
LongarMD <mark.longar@gmail.com>
2019-02-11 18:46:04 +0100

    Delete SecretKeys.py

--- Code/SecretKeys.py
+++
@@ -1,8 +0,0 @@
-botKey = "NTQ0NTQ3MzY1MDUxMzAxOTA4.D0MtVw.78PRCDobjL32N9QlwNmUXRxs03E"
-
-twitter_consumerKey = "KnmatbswWwqA009XbAE0R0Ttt"
-twitter_consumerSecret = "MgzZx3O6oQtw1lcOO1ecvxCJF9HvN2j9GvXhPkLqbPbYUj39ee"
-twitter_accessToken = "3347850023-EAZgMaHyk9PX5tcvwQh6cKWUl9saKmg4OKdMVwS"
-twitter_tokenSecret = "IZ5tvuE3hpZpejoPpWBgvsP0AcFwZVHOE78QQOXdd6HHb"
-
-server_secretKey = "fABYpEn3PLe9OE0F7Yw"
