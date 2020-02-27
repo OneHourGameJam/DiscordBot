0b298060c229cb643716bc527a6ff74abebe8b2b
LongarMD <root@2exi.c.time4vps.cloud>
2020-02-27 22:55:50 +0200

    Adds hyoe and mc smook support along with some snacks for the bot

--- DiscordBot/commands/easter_eggs.py
+++ DiscordBot/commands/easter_eggs.py
@@ -11,3 +11,15 @@ class EasterEggs(commands.Cog):
     async def hype(self, ctx):
         hype_links = self.bot.file_manager.get_config('easter_eggs')['hype_links']
         await ctx.send(random.choice(hype_links))
+
+    @command()
+    async def hyoe(self, ctx):
+        await ctx.send("I am very excitement too:)")
+
+    @command(aliases=["botsnack", "BotSnack", "snack", "Snack"])
+    async def botSnack(self, ctx):
+        if ctx.message.author.id == 255517263724150787:
+            await ctx.send("I have type II diabetes, you know that I can't eat that, Liam.")
+
+        else:
+            await ctx.send("I don't take candy from strangers!")
